d2babf9b7e14b31d2964f4afc1c9f0896a4698a0
iwagen0321 <chupachups1019@i.softbank.jp>
2023-07-27 15:46:08 +0900

    login画面修正

--- routes/auth.php
+++ routes/auth.php
@@ -36,6 +36,15 @@ Route::middleware('guest')->group(function () {
 });
 
 Route::middleware('auth')->group(function () {
+    Route::get('register', [RegisteredUserController::class, 'create'])
+                ->name('register');
+
+    Route::post('register', [RegisteredUserController::class, 'store']);
+
+});
+
+
+Route::middleware('auth')->group(function () {
     Route::get('verify-email', EmailVerificationPromptController::class)
                 ->name('verification.notice');
 
